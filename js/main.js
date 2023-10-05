60c3a3599a8bf6844117c9cc27388cf9d45be449
wlduq0150 <wlduq0150@gmail.com>
2023-10-05 20:28:48 +0900

    update js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 var cards = document.querySelectorAll(".team_card");
 
-for ( var card of cards ) {
-    card.addEventListener("click", (event) => {
-        console.log("연결됨");
+const server = "";
+
+function getProfile(name) {
+    const response = fetch(server + "/profile", {
+        method: "GET"
     });
+    return response.json();
 }
+
+cards.forEach((card) => {
+    card.addEventListener("mouseover", (e) => {
+        var cardElement = e.currentTarget;
+
+        console.log(cardElement);
+
+        const name = e.target.value;
+        console.log(name);
+
+        // const { userName, mbti, intro } = getProfile();
+
+        
+
+        cardElement.style.width = "30%";
+        cardElement.style.height = "350px";
+
+        cardElement.querySelector(".mbti").style.display = "block";
+        cardElement.querySelector(".intro").style.display = "block";
+    });
+
+    card.addEventListener("mouseout", (e) => {
+        var cardElement = e.currentTarget;
+
+        cardElement.style.width = "20%";
+        cardElement.style.height = "280px";
+
+        cardElement.querySelector(".mbti").style.display = "none";
+        cardElement.querySelector(".intro").style.display = "none";
+    });
+});
